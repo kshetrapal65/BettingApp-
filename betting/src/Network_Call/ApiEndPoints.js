2f9f6e0426d9dd866c3c8109062abcca056a0abd
kshetrapal <kavya.kshetrapalsingh@gmail.com>
2024-11-14 18:33:09 +0530

    Props Implement

--- betting/src/Network_Call/ApiEndPoints.js
+++ betting/src/Network_Call/ApiEndPoints.js
@@ -4,7 +4,7 @@ const APPConfig = {
 
 const ApiEndPoints = {
   // ApiKey: "5269f50f7cb93fcfadbc3b2e991d2477",
-  ApiKey: "d9ad15d505b6cb0c7e925d3ca0899ef7",
+  ApiKey: "68521aca907caa4c16690e913e82ede8",
   Login: `${APPConfig.API_URL}/login`,
   Register: `${APPConfig.API_URL}/register`,
   ForgotPassword: `${APPConfig.API_URL}/forgot-password`,
